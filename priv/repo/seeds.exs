8d0fc49616b1c692beb3c76fddc484bda7466ac0
Angel Barrera Santana <barrerasantanaangel@comunidad.unam.mx>
2016-03-02 10:52:13 -0600

    Finished back-end

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,16 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias PhoenixTrello.{Repo, User}
+
+[
+  %{
+    first_name: "Angel",
+    last_name: "Barrera",
+    email: "angelbarrera@phoenix-trello.com",
+    password: "12345678"
+  },
+]
+|> Enum.map(&User.changeset(%User{}, &1))
+|> Enum.each(&Repo.insert!(&1))
--- web/static/js/routes/index.js
+++ web/static/js/routes/index.js
@@ -6,6 +6,7 @@ import HomeIndexView                from '../views/home';
 import RegistrationsNew             from '../views/registrations/new';
 import SessionsNew                  from '../views/sessions/new';
 import BoardsShowView               from '../views/boards/show';
+import CardsShowView               from '../views/cards/show';
 
 export default (
   <Route component={MainLayout}>
@@ -15,7 +16,71 @@ export default (
     <Route path="/" component={AuthenticatedContainer}>
       <IndexRoute component={HomeIndexView} />
 
-      <Route path="/boards/:id" component={BoardsShowView} />
+      <Route path="/boards/:id" component={BoardsShowView}>
+        <Route path="cards/:id" component={CardsShowView}/>
+      </Route>
     </Route>
   </Route>
 );
+import React, {PropTypes}   from 'react';
+import { connect }          from 'react-redux';
+import { Link }             from 'react-router';
+
+import { setDocumentTitle } from '../../utils';
+import Actions              from '../../actions/sessions';
+
+class SessionsNew extends React.Component {
+  componentDidMount() {
+    setDocumentTitle('Sign in');
+  }
+
+  _handleSubmit(e) {
+    e.preventDefault();
+
+    const { email, password } = this.refs;
+    const { dispatch } = this.props;
+
+    dispatch(Actions.signIn(email.value, password.value));
+  }
+
+  _renderError() {
+    const { error } = this.props;
+
+    if (!error) return false;
+
+    return (
+      <div className="error">
+        {error}
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className='view-container sessions new'>
+        <main>
+          <header>
+            <div className="logo" />
+          </header>
+          <form onSubmit={::this._handleSubmit}>
+            {::this._renderError()}
+            <div className="field">
+              <input ref="email" type="Email" placeholder="Email" required="true" defaultValue="john@phoenix-trello.com"/>
+            </div>
+            <div className="field">
+              <input ref="password" type="password" placeholder="Password" required="true" defaultValue="12345678"/>
+            </div>
+            <button type="submit">Sign in</button>
+          </form>
+          <Link to="/sign_up">Create new account</Link>
+        </main>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => (
+  state.session
+);
+
+export default connect(mapStateToProps)(SessionsNew);
